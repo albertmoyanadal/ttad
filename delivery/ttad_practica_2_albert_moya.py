096b77e0ec8ccd270dad65e05c170d19ba5fc3b7
Albert Moya <albertmoyanadal@gmail.com>
2025-11-17 21:10:02 +0100

    3b py

--- delivery/ttad_practica_2_albert_moya.py
+++ delivery/ttad_practica_2_albert_moya.py
@@ -3,6 +3,14 @@ def eliminaDecimales(df, col):
     return df
 
 
-
+def calculaRatio(df, col1, col2):
+    try:
+        ratio = df[col1] / df[col2] # això és una columna amb els ratios per a cada observació
+        if (ratio > 1).any(): # any() mira si hi ha al manco 1 observació
+            print("Hi ha algun valor major que 1")
+        return ratio
+    except:
+        print("Error!") # col2 pot tenir alguna observació que sigui 0!
+    
 
 
